83bad25013e8630536b049df3cb66ec9f6908d79
gbudisetiawan <abdiutama02@gmail.com>
2022-10-18 00:04:06 +0800

    Update Project

--- TUGAS 3 (13-10-2022)/Trivili/assets/script.js
+++
@@ -1,9 +0,0 @@
-//console.log("created by Gelgel");
-//const testing = document;
-//console.log(testing);
-
-const menuBar = document.querySelector(".menu-bar");
-const menuNav = document.querySelector(".menu-navigation");
-menuBar.addEventListener("click", function () {
-    menuNav.classList.toggle("active");
-});
