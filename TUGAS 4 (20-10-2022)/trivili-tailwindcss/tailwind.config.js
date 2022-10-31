c5ea79593000be2050c5c80547827ffeb0168667
odetv <abdiutama02@gmail.com>
2022-10-31 20:57:50 +0800

    Update file

--- TUGAS 4 (20-10-2022)/trivili-tailwindcss/tailwind.config.js
+++ TUGAS 4 (20-10-2022)/trivili-tailwindcss/tailwind.config.js
@@ -1,6 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ['index.html'],
+  content: ['index.html','dist/pages'],
   theme: {
     container: {
       center: true,
